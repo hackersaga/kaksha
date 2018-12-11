8969b1016cfc1264d2a87398df263f5672133106
Sagar Patidar <patidar.sagar6955@gmail.com>
2018-12-11 15:36:11 +0530

    Kaksha App v1

---
+++ app/src/main/java/in/parentto/kaksha/DriveWebviewActivity.java
@@ -0,0 +1,34 @@
+package in.parentto.kaksha;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class DriveWebviewActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drive_webview);
+
+        getSupportActionBar().hide();
+
+        Intent intent = getIntent();
+        String url = intent.getStringExtra("url");
+        if(null == url){
+            url = "www.google.com";
+        }
+        webView = (WebView) findViewById(R.id.webView);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(url);
+//        webView.setHorizontalScrollBarEnabled(false);
+//        webView.over
+    }
+}
